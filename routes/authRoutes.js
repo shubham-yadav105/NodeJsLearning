cf5659c3dd0635f2840ec56011b225834fb25b03
shubham Yadav <sy907219@gmail.com>
2026-06-08 22:52:32 +0530

    routes Updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
+const validate = require("../middleware/validate");
+const { registerSchema, loginSchema } = require("../validators/authValidator");
 
-router.post("/register", register);
-router.post("/login", login);
+// validate middleware runs BEFORE constroller - like FormRequest in Laravel 
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 module.exports = router;
