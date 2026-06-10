7c1f0e77e7bf761a666369678880e5513e860b61
shubham Yadav <sy907219@gmail.com>
2026-06-10 08:36:00 +0530

    Avatar added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient(); // like using the DB facade in Laravel
+const path = require("path");
+const fs = require("fs");   // build-in Node.js - like PHP's unlink()
+
 
 // GET /users - like User::all()
 const getAllUsers = async (req, res) => {
@@ -88,6 +91,61 @@ const deleteUser = async (req, res) => {
 
         res.json({ message: "User deleted successfully" });
     } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// POST /users/avatar - upload profile picture
+const uploadAvatar = async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: "Please upload an image" });
+        }
+
+        const userId = req.user.id; // from JWT token 
+
+        //Check if user already has an avatar - delete old one
+        const user = await prisma.user.findUnique({ where: { id: userId } });
+
+        if (user.avatar) {
+            const oldPath = path.join(__dirname, "..", user.avatar);
+            if (fs.existsSync(oldPath)) {
+                fs.unlinkSync(oldPath); // like Storage::delete() in Laravel
+            }
+        }
+
+        // Save new avatar path to database 
+        const avatarPath = "uploads/" + req.file.filename;
+
+        const updatedUser = await prisma.user.update({
+            where: { id: userId },
+            data: { avatar: avatarPath },
+            select: { id: true, name: true, email: true, avatar: true }
+        });
+
+        res.json({
+            message: "Avatar uploaded successfully",
+            user: updateUser
+        });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// GET /users/avatr/:filename - serve the image
+
+const getAvatar = async (req, res) => {
+    try {
+        const filePath = path.join(__dirname, "..", "upload", req.params.filename);
+
+        if (!fs.existsSync(filePath)) {
+            return res.status(404).json({ message: "Image not found" });
+        }
+
+        res.sendFile(filePath); // like Storage::url() in Laravel
+    }
+    catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
